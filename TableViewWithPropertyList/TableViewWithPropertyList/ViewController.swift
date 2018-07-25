0c1c1bba20545c2090fa5423b27c903a53d1e094
kurokyfan <kuro2007cumt@126.com>
2018-07-25 16:23:01 +0800

    extension update

--- TableViewWithPropertyList/TableViewWithPropertyList/ViewController.swift
+++ TableViewWithPropertyList/TableViewWithPropertyList/ViewController.swift
@@ -8,26 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
     
-    var cells : NSDictionary? // Global Variable
+    var dicData : NSDictionary? // Global Variable
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupData()
+        setupTableView()
+    }
+    
+    func setupData() {
         let path = Bundle.main.bundlePath
         let plistName:NSString = "Property List.plist"
         let finalPath:NSString = (path as NSString).appendingPathComponent(plistName as String) as NSString
-        cells = NSDictionary(contentsOfFile:finalPath as String)
+        dicData = NSDictionary(contentsOfFile:finalPath as String)
     }
     
+    func setupTableView() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cells!.count
+        return dicData!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
         
-        let cell:CustomCell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
-        let myCell: AnyObject = cells!.object(forKey: "cell\((indexPath as NSIndexPath).row)") as! NSDictionary
+        let cell: CustomCell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        let myCell: AnyObject = dicData!.object(forKey: "cell\((indexPath as NSIndexPath).row)") as! NSDictionary
         
         cell.myTitle?.text = myCell.object(forKey: "title") as? String
         cell.mySubtitle?.text = myCell.object(forKey: "subtitle") as? String
@@ -43,9 +61,4 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, titleForFooterInSection section:Int) -> String? {
         return "Get all the Tux"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
 }
